81092e40da3e8e93f8fce80cb95f746a10b42de6
bit1394 <it_nv@mail.ru>
2014-12-25 19:29:05 +0400

    First app

---
+++ x1-android/gen/com/x1/game/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.x1.game.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ x1-android/src/com/x1/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.x1.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.x1.game.x1;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new x1(), config);
+	}
+}
